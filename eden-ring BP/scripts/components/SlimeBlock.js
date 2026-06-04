0d8672fbd6970720a68847b116010f2347f60f6a
Halo333X <124174275+Halo333X@users.noreply.github.com>
2026-06-04 12:07:51 -0600

    Symbiotic Mold Block

--- eden-ring BP/scripts/components/SlimeBlock.js
+++ eden-ring BP/scripts/components/SlimeBlock.js
@@ -1,9 +1,11 @@
 export const slimeBlockComponent = {
-    onEntityFallOn({ block, entity }, { params }) {
+    onStepOn({ block, entity }, { params }) {
         const p = params;
-        const speed = entity?.getDynamicProperty('edenring:speed');
-        const bound = Math.abs(speed.y ? speed.y : 0) * p.bound;
-        const force = { x: 0, y: bound < 0.3 ? 0 : bound, z: 0 };
-        entity?.applyImpulse(force);
+        const velocity = entity?.getVelocity();
+        if (!velocity)
+            return;
+        if (velocity?.y < 0) {
+            entity?.applyKnockback({ x: 0, z: 0 }, Math.abs(velocity.y));
+        }
     }
 };
--- eden-ring BP/src/components/SlimeBlock.ts
+++ eden-ring BP/src/components/SlimeBlock.ts
@@ -1,12 +1,12 @@
 import { Block, BlockCustomComponent, Vector3 } from "@minecraft/server";
 
 export const slimeBlockComponent: BlockCustomComponent = {
-    onEntityFallOn({ block, entity }, { params }) {
+    onStepOn({ block, entity }, { params }) {
         const p = params as { bound: number };
-        const speed = entity?.getDynamicProperty('edenring:speed') as Vector3;
-        const bound = Math.abs(speed.y ? speed.y : 0) * p.bound;
-        const force = { x: 0, y: bound < 0.3 ? 0 : bound, z: 0 };
-        entity?.applyImpulse(force);
+        const velocity = entity?.getVelocity();
+        if (!velocity) return;
+        if (velocity?.y < 0) {
+            entity?.applyKnockback({ x: 0, z: 0 }, Math.abs(velocity.y));
+        }
     }
 };
-
